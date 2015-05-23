8cf01589bc6a9851c301e5de0171e41201927348
Loredana Cirstea <loredana.cirstea@gmail.com>
2015-05-23 11:36:24 +0300

    a change

--- push.sh
+++ push.sh
@@ -3,9 +3,9 @@
 # modify the script if this is not the case
  
 meteor build prod
-cp prod/meteor-svg-app.tar.gz ~/openshift
+cp prod/meteor-svg-app.tar.gz ~/oroboro
 rm prod/meteor-svg-app.tar.gz
-cd ~/openshift
+cd ~/oroboro
 tar -xvf meteor-svg-app.tar.gz -s '/^bundle//'
 rm meteor-svg-app.tar.gz
 git add .
