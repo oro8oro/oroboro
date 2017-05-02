f5fcee732a421fbb3bbe354b23ffb2b9cd0905a0
Loredana Cirstea <loredana.cirstea@gmail.com>
2017-05-02 22:15:54 +0300

    Fixed update in editor + fill color

--- client/js/wraps.js
+++ client/js/wraps.js
@@ -1,5 +1,6 @@
 global_oro_variables.wraps = {
   update_collection: function(collection, ids, upd, callb) {
+    var fileId = Session.get('fileId');
     Meteor.call('update_collection', collection, ids, upd, function(err, res) {
       if(callb) callb(err, res);
       Meteor.call('unset_document', 'File', fileId, ['svg']);
